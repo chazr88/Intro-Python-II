f036249fa87eed66ee16cdf080396ffc15f1b370
Chaz Rowe <chazr_88@hotmail.com>
2020-01-23 23:27:58 -0500

    got some of the basics working

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,25 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    into the darkness. Ahead to the north, a light flickers in
+    the distance, but there is no way across the chasm."""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    chamber! Sadly, it has already been completely emptied by
+    earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -38,7 +39,8 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+new_player = Player("Chaz", Room("outside", "brown"))
+print(new_player)
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +51,9 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+selection = ""
+while selection is not "q":
+    selection = input(f"Please choose n, s, e, w to move your player in that direction to a new room: ")
+    
+    if selection == "n":
+        print("Yay")
--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def __str__(self):
+        return self.name
+
+    def get_color(self):
+        return self.color
